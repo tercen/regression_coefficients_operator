d4d3b15d0a078a9414abccf36d624da7bcfd7340
agouy <contact@agouy.com>
2022-04-11 12:16:07 +0000

    added perc variance explained

--- main.R
+++ main.R
@@ -1,5 +1,7 @@
 library(tercen)
+library(tercenApi)
 library(dplyr, warn.conflicts = FALSE)
+library(purrr)
 library(broom)
 
 get.coefficients <- function(df) {
@@ -13,12 +15,12 @@ get.coefficients <- function(df) {
   coefs[["log10_p_value"]] <- -log10(coefs[["p_value"]])
   coefs[[".ri"]] <- df[[".ri"]][1]
   
-  # perc_var <- broom::tidy(anova(m)) %>% rename(covariate = term)
-  # perc_var[["perc_variance"]] <- 100 * perc_var$sumsq / sum(perc_var$sumsq)
-  # perc_var <- select(perc_var, perc_variance, covariate)
+  perc_var <- broom::tidy(anova(m)) %>% rename(variable = term)
+  perc_var[["perc_variance"]] <- 100 * perc_var$sumsq / sum(perc_var$sumsq)
+  perc_var <- select(perc_var, perc_variance, variable)
+  perc_var[[".ri"]] <- df[[".ri"]][1]
   
-  df_out <- coefs# left_join(coefs, perc_var, "covariate")
-  return(df_out)
+  return(list(coefs, perc_var))
 }
 
 ctx = tercenCtx()
@@ -29,9 +31,15 @@ df_out <- ctx %>%
   select(.y, .ri, .ci) %>%
   left_join(covariates, ".ci") %>%
   select(-.ci) %>%
-  group_by(.ri) %>%
-  do(get.coefficients(.)) %>%
+  split(.$.ri) %>%
+  map(~get.coefficients(.))
+
+coefs <- lapply(df_out, "[[", 1) %>%
+  bind_rows() %>%
   ctx$addNamespace() 
 
-df_out %>%
-  ctx$save()
+perc_var <- lapply(df_out, "[[", 2) %>%
+  bind_rows() %>%
+  ctx$addNamespace() 
+
+ctx$save(list(coefs, perc_var))
